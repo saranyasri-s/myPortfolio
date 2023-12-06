9cd8f4ebf5b5512c521932d4a1c6c0b4003bb157
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-12-06 17:26:22 +0530

    added css to navbar

--- src/components/navbar/NavItems/Navitems.js
+++ src/components/navbar/NavItems/Navitems.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./NavItems.module.css";
 function Navitems() {
   return (
-    <div>
+    <div className={classes.NavItems}>
       <h4>About</h4>
       <h4>Experience</h4>
       <h4>Projects</h4>
