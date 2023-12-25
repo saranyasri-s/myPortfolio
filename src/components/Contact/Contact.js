89bdb42980a20e8426391478767ae8da5cfb8abe
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-12-25 14:09:03 +0530

    added contact protion

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,39 @@
 import React from "react";
 import classes from "./Contact.module.css";
+import { AiTwotoneMail } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
 function Contact() {
   return (
     <div>
-      <div>
+      <div className={classes.AboutHeading}>
         <p>Get in touch</p>
-        <p>Contact me</p>
+        <h2>Contact me</h2>
       </div>
-      <div>
-        <div>dentist.apassionateprogrammer@gmail.com</div>
-        <div>LinkedIn</div>
+      <div className={classes.contact}>
+        <div>
+          <AiTwotoneMail
+            style={{ padding: "10px", height: "30px", width: "30px" }}
+          ></AiTwotoneMail>
+          <p
+            style={{
+              cursor: "pointer",
+              color: "grey",
+              fontSize: "1.2rem",
+              fontWeight: "500",
+            }}
+          >
+            dentist.apassionateprogrammer@gmail.com
+          </p>
+        </div>
+
+        <div>
+          <FaLinkedin
+            style={{ padding: "10px", height: "30px", width: "30px" }}
+          ></FaLinkedin>
+          <p style={{ cursor: "pointer", color: "grey", fontSize: "1.2rem" }}>
+            LinkedIn
+          </p>
+        </div>
       </div>
     </div>
   );
